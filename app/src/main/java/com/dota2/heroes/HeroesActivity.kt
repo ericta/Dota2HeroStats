db430f7cae0a38f7368b31c35ece3f866a4b3d2f
EricTa <Ericta90@gmail.com>
2018-10-17 13:29:14 +0700

    init source

---
+++ app/src/main/java/com/dota2/heroes/HeroesActivity.kt
@@ -0,0 +1,13 @@
+package com.dota2.heroes
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HeroesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_heroes)
+    }
+
+}
